f55606d6b0d7f0002d8298789eb8a7f6b01446d3
Reyna <hh1864@wayne.edu>
2026-01-08 17:49:03 -0500

    Replace site with reactfolio template

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import { useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import ReactGA from "react-ga4";
+
+import Homepage from "./assets/pages/homepage";
+import About from "./assets/pages/about";
+import Projects from "./assets/pages/projects";
+import Articles from "./assets/pages/articles";
+import ReadArticle from "./assets/pages/readArticle";
+import Contact from "./assets/pages/contact";
+import Notfound from "./assets/pages/404";
+
+import { TRACKING_ID } from "./assets/data/tracking";
+import "./app.css";
+
+function App() {
+	useEffect(() => {
+		if (TRACKING_ID !== "") {
+			ReactGA.initialize(TRACKING_ID);
+		}
+	}, []);
+
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Homepage />} />
+				<Route path="/about" element={<About />} />
+				<Route path="/projects" element={<Projects />} />
+				<Route path="/articles" element={<Articles />} />
+				<Route path="/article/:slug" element={<ReadArticle />} />
+				<Route path="/contact" element={<Contact />} />
+				<Route path="*" element={<Notfound />} />
+			</Routes>
+		</div>
+	);
+}
+
+export default App;
