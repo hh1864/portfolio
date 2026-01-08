731d6cc6c88488dfc3324d2546f5865030b3e806
Reyna <hh1864@wayne.edu>
2026-01-08 18:51:30 -0500

    Remove debug step from GitHub Actions deploy workflow

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,9 @@
-import { Routes, Route } from "react-router-dom";
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 
-import Homepage from "./pages/homepage";
-import About from "./pages/about";
-import Projects from "./pages/projects";
-import Articles from "./pages/articles";
-import ReadArticle from "./pages/readArticle";
-import Contact from "./pages/contact";
-import Notfound from "./pages/404";
 
-import "./app.css";
-
-function App() {
-	return (
-		<div className="App">
-			<Routes>
-				<Route path="/" element={<Homepage />} />
-				<Route path="/about" element={<About />} />
-				<Route path="/projects" element={<Projects />} />
-				<Route path="/articles" element={<Articles />} />
-				<Route path="/article/:slug" element={<ReadArticle />} />
-				<Route path="/contact" element={<Contact />} />
-				<Route path="*" element={<Notfound />} />
-			</Routes>
-		</div>
-	);
+import Homepage from "./pages/homepage.jsx";
+export default function App() {
+  return <Homepage />;
 }
-
-export default App
