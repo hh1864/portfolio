b26d327aec8f3858809075440efc4e1ade3d2ee5
Reyna <hh1864@wayne.edu>
2026-01-08 19:02:02 -0500

    fixing broken layout and topbar not working,

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-
+import { Routes, Route } from "react-router-dom";
 
 import Homepage from "./pages/homepage.jsx";
+import About from "./pages/about.jsx";
+import Projects from "./pages/projects.jsx";
+import Articles from "./pages/articles.jsx";
+import Contact from "./pages/contact.jsx";
+import ReadArticle from "./pages/readArticle.jsx";
+import NotFound from "./pages/404.jsx";
+
+import "./app.css";
+
 export default function App() {
-  return <Homepage />;
+	return (
+		<Routes>
+			<Route path="/" element={<Homepage />} />
+			<Route path="/about" element={<About />} />
+			<Route path="/projects" element={<Projects />} />
+			<Route path="/articles" element={<Articles />} />
+			<Route path="/article/:slug" element={<ReadArticle />} />
+			<Route path="/contact" element={<Contact />} />
+			<Route path="*" element={<NotFound />} />
+		</Routes>
+	);
 }
